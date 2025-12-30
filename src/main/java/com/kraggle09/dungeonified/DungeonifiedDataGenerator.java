871ca211d740c1426f7b9b5c9ab8e51ba4d2718a
Kraggle09 <redstoneweeklypodcast@gmail.com>
2025-12-30 11:07:08 -0600

    Initial Commit

---
+++ src/main/java/com/kraggle09/dungeonified/DungeonifiedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kraggle09.dungeonified;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DungeonifiedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
