d94a4ef07b81207323d7bf518449b35cc6292cf9
Kraggle09 <redstoneweeklypodcast@gmail.com>
2026-01-12 09:29:38 -0600

    Moved pickaxe_mineable tags to data-gen system

--- src/main/java/com/kraggle09/dungeonified/datagen/ModBlockTagProvider.java
+++ src/main/java/com/kraggle09/dungeonified/datagen/ModBlockTagProvider.java
@@ -1,10 +1,15 @@
 package com.kraggle09.dungeonified.datagen;
 
+import com.kraggle09.dungeonified.Dungeonified;
 import com.kraggle09.dungeonified.blocks.ModBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -25,5 +30,54 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
                 .add(ModBlocks.DRIPSTONE_BRICK_WALL)
                 .add(ModBlocks.STONE_QUARTZ_WALL)
                 .add(ModBlocks.STONE_QUARTZ_BRICK_WALL);
+        getOrCreateTagBuilder(STONE_TYPES)
+                .add(ModBlocks.RUNESTONE)
+                .add(ModBlocks.RUNESTONE_STAIRS)
+                .add(ModBlocks.RUNESTONE_SLAB)
+                .add(ModBlocks.RUNESTONE_WALL)
+                .add(ModBlocks.POLISHED_RUNESTONE)
+                .add(ModBlocks.POLISHED_RUNESTONE_STAIRS)
+                .add(ModBlocks.POLISHED_RUNESTONE_SLAB)
+                .add(ModBlocks.POLISHED_RUNESTONE_WALL)
+                .add(ModBlocks.PATTERNED_RUNESTONE)
+                .add(ModBlocks.PATTERNED_RUNESTONE_STAIRS)
+                .add(ModBlocks.PATTERNED_RUNESTONE_SLAB)
+                .add(ModBlocks.PATTERNED_RUNESTONE_WALL)
+                .add(ModBlocks.CHISELED_RUNESTONE)
+                .add(ModBlocks.RUNESTONE_BRICKS)
+                .add(ModBlocks.RUNESTONE_BRICK_STAIRS)
+                .add(ModBlocks.RUNESTONE_BRICK_SLAB)
+                .add(ModBlocks.RUNESTONE_BRICK_WALL)
+                .add(ModBlocks.SQUARED_RUNESTONE_BRICKS)
+                .add(ModBlocks.SQUARED_RUNESTONE_BRICK_STAIRS)
+                .add(ModBlocks.SQUARED_RUNESTONE_BRICK_SLAB)
+                .add(ModBlocks.SQUARED_RUNESTONE_BRICK_WALL)
+                .add(ModBlocks.CHISELED_RUNESTONE_BRICKS)
+                .add(ModBlocks.MORTISED_RUNESTONE_BRICKS)
+                .add(ModBlocks.RUNESTONE_PILLAR)
+                .add(ModBlocks.POLISHED_DRIPSTONE)
+                .add(ModBlocks.POLISHED_DRIPSTONE_STAIRS)
+                .add(ModBlocks.POLISHED_DRIPSTONE_SLAB)
+                .add(ModBlocks.POLISHED_DRIPSTONE_WALL)
+                .add(ModBlocks.DRIPSTONE_BRICKS)
+                .add(ModBlocks.DRIPSTONE_BRICK_STAIRS)
+                .add(ModBlocks.DRIPSTONE_BRICK_SLAB)
+                .add(ModBlocks.DRIPSTONE_BRICK_WALL)
+                .add(ModBlocks.CHISELED_DRIPSTONE)
+                .add(ModBlocks.STONE_QUARTZ)
+                .add(ModBlocks.STONE_QUARTZ_SLAB)
+                .add(ModBlocks.STONE_QUARTZ_STAIRS)
+                .add(ModBlocks.STONE_QUARTZ_WALL)
+                .add(ModBlocks.CHISELED_STONE_QUARTZ)
+                .add(ModBlocks.STONE_QUARTZ_BRICKS)
+                .add(ModBlocks.STONE_QUARTZ_BRICK_STAIRS)
+                .add(ModBlocks.STONE_QUARTZ_BRICK_SLAB)
+                .add(ModBlocks.STONE_QUARTZ_BRICK_WALL)
+                .add(ModBlocks.CHISELED_STONE_QUARTZ_BRICKS)
+                .add(ModBlocks.STONE_QUARTZ_PILLAR);
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .addTag(STONE_TYPES);
     }
+
+    public static final TagKey<Block> STONE_TYPES = TagKey.of(RegistryKeys.BLOCK, Identifier.of(Dungeonified.MOD_ID, "stone_types"));
 }
